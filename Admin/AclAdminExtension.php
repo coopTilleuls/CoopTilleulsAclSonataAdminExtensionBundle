58d113e5d2aa537aa5fd7bda7519a4d36c025da2
MrGreenStuff <juillard.yoann@gmail.com>
2014-01-09 00:19:10 +0100

    Update AclAdminExtension.php

--- Admin/AclAdminExtension.php
+++ Admin/AclAdminExtension.php
@@ -5,6 +5,9 @@
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
+ *
+ *
+ * Enhanced by JUILLARD Yoann
  */
 
 namespace CoopTilleuls\Bundle\AclSonataAdminExtensionBundle\Admin;
@@ -21,6 +24,8 @@ use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
  * Admin extension filtering the list
  *
  * @author Kévin Dunglas <kevin@les-tilleuls.coop>
+ * 
+ * Enhanced By JUILLARD Yoann
  */
 class AclAdminExtension extends AdminExtension
 {
@@ -78,7 +83,6 @@ class AclAdminExtension extends AdminExtension
         $classStmt->execute();
 
         $classId = $classStmt->fetchColumn();
-
         if ($identityId && $classId) {
             $entriesStmt = $this->databaseConnection->prepare('SELECT object_identifier FROM acl_entries AS ae JOIN acl_object_identities AS aoi ON ae.object_identity_id = aoi.id WHERE ae.class_id = :classId AND ae.security_identity_id = :identityId AND (:view = ae.mask & :view OR :operator = ae.mask & :operator OR :master = ae.mask & :master OR :owner = ae.mask & :owner)');
             $entriesStmt->bindValue('classId', $classId);
@@ -93,18 +97,74 @@ class AclAdminExtension extends AdminExtension
             foreach ($entriesStmt->fetchAll() as $row) {
                 $ids[] = $row['object_identifier'];
             }
+			//IF THERE IS NOT DIRECT RESULT WE MADE A QUERY ON THE MASTER ACL CLASS
+			//Test if method getMasterACLclass and getPathToMasterACL exist on the admin CLASS -> SEE THE DOC
+            if (method_exists($admin,'getMasterACLclass') && method_exists($admin,'getPathToMasterACL')) {
+				$classStmt = $this->databaseConnection->prepare('SELECT id FROM acl_classes WHERE class_type = :classType');
+				//QUERY ON MASTER ACL CLASS (method $admin->getMasterACLclass() return a string like 'Acme\Bundle\Entity\MasterACLEntity');
+				$classStmt->bindValue('classType', $admin->getMasterACLclass());
+				$classStmt->execute();
 
-            if (count($ids)) {
-                $query
+				$classId = $classStmt->fetchColumn();
+				$entriesStmt = $this->databaseConnection->prepare('SELECT object_identifier FROM acl_entries AS ae JOIN acl_object_identities AS aoi ON ae.object_identity_id = aoi.id WHERE ae.class_id = :classId AND ae.security_identity_id = :identityId AND (:view = ae.mask & :view OR :operator = ae.mask & :operator OR :master = ae.mask & :master OR :owner = ae.mask & :owner)');
+				$entriesStmt->bindValue('classId', $classId);
+				$entriesStmt->bindValue('identityId', $identityId);
+				$entriesStmt->bindValue('view', MaskBuilder::MASK_VIEW);
+				$entriesStmt->bindValue('operator', MaskBuilder::MASK_OPERATOR);
+				$entriesStmt->bindValue('master', MaskBuilder::MASK_MASTER);
+				$entriesStmt->bindValue('owner', MaskBuilder::MASK_OWNER);
+				$entriesStmt->execute();
+				//ARRAY OF idsMaster
+				$idsMaster = array();
+				foreach ($entriesStmt->fetchAll() as $row) {
+					$idsMaster[] = $row['object_identifier'];
+				}
+				/*The method $admin->getPathToMasterACL() have to return an array (BE CAREFULL OF ORDER) like : 
+					array(
+						array('firstChild','c1'),
+						array('secondChild','c2'),
+						array('thirdChild','c3')
+						...
+					)
+					where the first argument is the name of the property relation and second a unique selector (TODO MADE NAME SELECTOR AUTOMATIC)
+				*/
+				$parents=$admin->getPathToMasterACL();
+				//HERE UPDATE THE QUERY
+				foreach($parents as $key=>$parent){
+					//FIRST shorcut is 'o'
+					if($key==0){
+						$query->leftJoin('o.'.$parent[0],$parent[1]);
+					}else{
+					//Shortcut is precedent shortcut
+						$query->leftJoin($parents[$key-1][1].'.'.$parent[0],$parent[1]);
+					}
+					//HERE WE ARE AFTER THE LEFT JOIN ON MASTER ACL CLASS WE PASS idsMaster array param
+					if(($key+1)==count($parents)){
+						//HERE FOR OBJECT CREATED BY CURRENT USER
+						if(count($ids)){
+							//création de l'expression OR EXPRESSION
+							$orCondition = $query->expr()->orx();
+							$orCondition->add($query->expr()->in('o.id', ':ids'));
+							$orCondition->add($query->expr()->in($parent[1].'.id',':idsMaster'));
+							$query->andWhere($orCondition)->setParameter('ids', $ids)->setParameter('idsMaster', $idsMaster);
+						}else{
+							$query->andWhere($parent[1].'.id IN (:idsMaster'.$key.')')->setParameter('idsMaster'.$key, $idsMaster);
+						}
+					}
+				}
+				return;
+            }elseif(count($ids)){
+				//NORMAL BEHAVIOR
+				$query
                     ->andWhere('o.id IN (:ids)')
                     ->setParameter('ids', $ids)
                 ;
-
                 return;
-            }
+			}
         }
-
-        // Display an empty list
-        $query->andWhere('1 = 2');
+		
+		
+		// Display an empty list
+		$query->andWhere('1 = 2');
     }
 }
