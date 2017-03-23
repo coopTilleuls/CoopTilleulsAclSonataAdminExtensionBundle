cc936e6ca9a47b7afb582727699c5b95d85c128d
vedro-compota <ru-ibn-hottab@yandex.ru>
2017-03-23 19:04:29 +0300

    code style fixes

--- DependencyInjection/CoopTilleulsAclSonataAdminExtensionExtension.php
+++ DependencyInjection/CoopTilleulsAclSonataAdminExtensionExtension.php
@@ -13,7 +13,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -34,13 +33,17 @@ class CoopTilleulsAclSonataAdminExtensionExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        
-        $definition = $container->getDefinition('coop_tilleuls_acl_sonata_admin_extension.acl.extension');
-        
-        if (interface_exists(TokenStorageInterface::class)) {
-            $definition->replaceArgument(0 ,new Reference('security.token_storage'));  
-        } else {
-            $definition->replaceArgument(0, new Reference('security.context'));
-        }
+
+        $container
+            ->getDefinition('coop_tilleuls_acl_sonata_admin_extension.acl.extension')
+            ->replaceArgument(
+                0,
+                new Reference(
+                    interface_exists('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface')
+                        ? 'security.token_storage'
+                        : 'security.context'
+                )
+            )
+        ;  
     }
 }
